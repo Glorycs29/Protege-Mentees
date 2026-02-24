5897d72cd3dfb9b40778bfc874ae30a11c677fea
akshaytheflash <akshayrajhans053@gmail.com>
2026-02-24 08:18:49 -0800

    Create selection.cpp
    Signed-off-by: akshaytheflash <akshayrajhans053@gmail.com>

---
+++ Akshay/sorting/selection.cpp
@@ -0,0 +1,28 @@
+// Link: https://www.geeksforgeeks.org/problems/selection-sort/1
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void selectionSort(vector<int>& v){
+    int n = v.size();
+    for(int i = 0; i < n - 1; i++){
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(v[j] < v[minIndex])
+                minIndex = j;
+        }
+
+        if(minIndex != i)
+            swap(v[i], v[minIndex]);
+    }
+}
+
+
+int main(){
+    vector<int> v = {};
+    selectionSort(v);
+    for(auto i : v)
+        cout << i << " ";
+
+    return 0;
+}
