927cef9643e5d77b437d0c92fb27143e547413f0
akshaytheflash <akshayrajhans053@gmail.com>
2026-02-24 08:17:14 -0800

    Create StringConcatenatedFromelEmentsOfAnArray.cpp
    Signed-off-by: akshaytheflash <akshayrajhans053@gmail.com>

---
+++ Akshay/selection/strings/StringConcatenatedFromelEmentsOfAnArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     static bool comp(string a, string b){
+        return a + b > b + a;
+    }   
+
+    string largestNumber(vector<int>& nums) {
+        vector<string> numsstr;
+        
+        for (auto it : nums){
+            numsstr.push_back(to_string(it));
+        }
+        sort(numsstr.begin(), numsstr.end(), comp);
+        if (numsstr[0] == "0") return "0";
+
+        string ans = "";
+        for (int i = 0; i<numsstr.size(); i++){
+            ans += numsstr[i];
+        }
+        return ans;
+    }
+};
